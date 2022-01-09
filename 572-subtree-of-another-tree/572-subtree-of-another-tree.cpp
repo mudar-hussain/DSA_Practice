31ca7b368bb5b95097106c3ba4de55bede29eda6
Mudar Hussain <v448474@gmail.com>
2022-01-09 13:08:18 +0530

    Time: 19 ms (81.77%), Space: 28.9 MB (21.59%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -17,30 +17,30 @@ public:
         
         return (p->val == q->val && identical(p->left,q->left) && identical(p->right,q->right));
     }
-     // bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-     //     if(!root && !subRoot) return true;
-     //     if(!root || !subRoot) return false;
-     //     if(identical(root,subRoot)) return true;
-     //     return ( isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot));
-     // }
+     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+         if(!root && !subRoot) return true;
+         if(!root || !subRoot) return false;
+         if(identical(root,subRoot)) return true;
+         return ( isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot));
+     }
     
     
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+//     bool isSubtree(TreeNode* root, TreeNode* subRoot) {
         
-        if(!root && !subRoot) return true;
-        if(!root || !subRoot) return false;
-        queue<TreeNode *> q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* temp = q.front();
-            q.pop();
-            if(temp->val == subRoot->val)
-                if(identical(temp,subRoot)){
-                    return true;
-                }
-            if(temp->left) q.push(temp->left);
-            if(temp->right) q.push(temp->right);
-        }
-        return false;
-    }
+//         if(!root && !subRoot) return true;
+//         if(!root || !subRoot) return false;
+//         queue<TreeNode *> q;
+//         q.push(root);
+//         while(!q.empty()){
+//             TreeNode* temp = q.front();
+//             q.pop();
+//             if(temp->val == subRoot->val)
+//                 if(identical(temp,subRoot)){
+//                     return true;
+//                 }
+//             if(temp->left) q.push(temp->left);
+//             if(temp->right) q.push(temp->right);
+//         }
+//         return false;
+//     }
 };
