f5b520ab924b648b44721292ca5addd254891239
Mudar Hussain <v448474@gmail.com>
2022-01-10 11:50:59 +0530

    Time: 12 ms (95.17%), Space: 14.3 MB (28.17%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,49 +9,12 @@
  */
 class Solution {
 public:
-    TreeNode* ans;
-    bool isthere(TreeNode *root, TreeNode* p, TreeNode* q){
-        if(!root) return false;
-        if(root->val == p->val || root->val == q->val){
-            return true;
-        }
-        return (isthere(root->left,p,q) || isthere(root->right,p,q));
-    }
-    
-    void solve(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(ans) cout<<ans->val<<" ";
-        if(!root || ans) return;
-        if(root->val == p->val || root->val == q->val){
-            if(isthere(root->left,p,q) || isthere(root->right,p,q)){
-                ans = root;
-                return;
-            }
-        }
-        if(root->left && root->right){
-            if(isthere(root->left,p,q) && isthere(root->right,p,q)) {
-                ans = root;
-                return;
-            }
-        }
-        if(root->left){
-            if(isthere(root->left,p,q)){
-                solve(root->left,p,q);
-                return;
-            }
-            
-        }
-        if(root->right){
-            if(isthere(root->right,p,q)){
-                solve(root->right,p,q);
-                return;
-            }
-        }
-    }
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root) return root;
-        solve(root,p,q);
-        return ans;
+        if (!root || root == p || root == q) return root;
         
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) return root;
+        else return left? left : right;
     }
 };
