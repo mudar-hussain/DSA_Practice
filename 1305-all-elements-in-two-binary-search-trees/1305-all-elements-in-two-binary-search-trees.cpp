9288b76ffc3301902536bf856989921fd720051f
Mudar Hussain <v448474@gmail.com>
2022-02-19 10:14:08 +0530

    Time: 287 ms (17.57%), Space: 84.4 MB (79.01%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,18 +11,57 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode *root, vector<int> &ans){
+    void pushleft(TreeNode *root, stack<TreeNode*> &s){
         if(root==NULL) return;
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
-        
+        while(root!=NULL){
+            s.push(root);
+            root = root->left;
+        }
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
         vector<int> ans;
-        inorder(root1, ans);
-        inorder(root2, ans);
-        sort(ans.begin(), ans.end());
+        stack<TreeNode*> s1, s2;
+        pushleft(root1, s1);
+        pushleft(root2, s2);
+        while(!s1.empty() && !s2.empty()){
+            int x= s1.top()->val;
+            int y = s2.top()->val;
+            if(x<y){
+                ans.push_back(x);
+                TreeNode *temp = s1.top();
+                s1.pop();
+                pushleft(temp->right, s1);
+            }
+            else if(x>y){
+                ans.push_back(y);
+                TreeNode *temp = s2.top();
+                s2.pop();
+                pushleft(temp->right, s2);
+            }
+            else {
+                ans.push_back(x);
+                TreeNode *temp1 = s1.top();
+                s1.pop();
+                pushleft(temp1->right, s1);
+                
+                ans.push_back(y);
+                TreeNode *temp = s2.top();
+                s2.pop();
+                pushleft(temp->right, s2);
+            }
+        }
+        while(!s1.empty()){
+            ans.push_back(s1.top()->val);
+            TreeNode *temp = s1.top();
+            s1.pop();
+            pushleft(temp->right, s1);
+        }
+        while(!s2.empty()){
+            ans.push_back(s2.top()->val);
+            TreeNode *temp = s2.top();
+            s2.pop();
+            pushleft(temp->right, s2);
+        }
         return ans;
     }
 };
