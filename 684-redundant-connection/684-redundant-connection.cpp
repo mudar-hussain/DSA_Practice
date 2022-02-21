26fbb7b8c7a4da09fbdb6b43a768d4edd9352320
Mudar Hussain <v448474@gmail.com>
2022-02-21 17:49:30 +0530

    Time: 39 ms (7.73%), Space: 97.7 MB (5.01%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     int findParent(int n, vector<int> parent){
-        if(n == parent[n]) return n;
+        if(n == parent[n] || parent[n] == -1) return n;
         return findParent(parent[n], parent);
                                  
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int> parent(n+1,0);
-        vector<int> ans;
-        for(int i = 0; i<=n; i++)
-            parent[i] = i;
+        vector<int> parent(n+1,-1);
         for(auto node: edges){
             int n1 = node[0], n2 = node[1];
+            
             int p1 = findParent(n1, parent);
             int p2 = findParent(n2, parent);
             if(p1 == p2){
-                ans.push_back(n1);
-                ans.push_back(n2);
-                return ans;
+                return node;
             }else{
                 parent[p1] = p2;
             }
         }
-        return ans;
+        return {};
     }
 };
