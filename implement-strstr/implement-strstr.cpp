ca42c4d8e815fce0aae59bcbb50cdebdb06f6832
Mudar Hussain <v448474@gmail.com>
2022-03-01 18:00:25 +0530

    Time: 18 ms, Memory: 7 MB - LeetHub

---
+++ implement-strstr/implement-strstr.cpp
@@ -0,0 +1,8 @@
+#include <string.h>
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(needle.length() == 0) return 0;
+        return haystack.find(needle);
+    }
+};
