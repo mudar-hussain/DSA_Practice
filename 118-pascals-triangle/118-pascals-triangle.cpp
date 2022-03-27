dcaa5dfb4cf98a40eb525e5a1403858702dd5555
Mudar Hussain <v448474@gmail.com>
2022-03-27 14:27:21 +0530

    Time: 6 ms (7.70%), Space: 6.6 MB (33.21%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -5,8 +5,7 @@ public:
         ans.push_back({1});
         if(numRows==1) return ans;
         ans.push_back({1,1});
-        cout<<"1";
-        if(numRows == 2) return ans;
+        if(numRows==2) return ans;
         for(int i = 2; i<numRows; i++){
             vector<int> a;
             a.push_back(1);
