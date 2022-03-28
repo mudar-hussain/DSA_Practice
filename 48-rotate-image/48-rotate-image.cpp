cbc4e1c0515f5a481e1f04c273a1b11b4d3b39a5
Mudar Hussain <v448474@gmail.com>
2022-03-28 10:08:52 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.20%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<i; j++){
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-        for(int i = 0; i<n; i++)
-            reverse(matrix[i].begin(), matrix[i].end());
+        // for(int i = 0; i<n; i++){
+        //     for(int j = 0; j<i; j++){
+        //         swap(matrix[i][j], matrix[j][i]);
+        //     }
+        // }
+        // for(int i = 0; i<n; i++)
+        //     reverse(matrix[i].begin(), matrix[i].end());
         
-//         int b = 0;
-//         for(int lr = 0, tb = n-1; lr<n-1; lr++,tb--){
-//             for(int rl = n-1, bt = 0; rl>0; rl--,bt++){
-//                 swap(matrix[lr][bt], matrix[tb][lr]);
-//                 swap(matrix[lr][tb], matrix[tb][rl]);
-//                 swap(matrix[lr][bt], matrix[bt][lr]);
-//             }
+        int b = 0;
+        cout<<matrix[0][0];
+        for(int i = 0,j = n-1; i<n/2; i++, j--){
+            for(int zn = i, nz = j; zn<n-1-i; zn++,nz--){
+                swap(matrix[i][zn], matrix[zn][j]);
+                cout<<matrix[i][zn];
+                swap(matrix[i][zn], matrix[j][nz]);
+                cout<<matrix[i][zn];
+                swap(matrix[i][zn], matrix[nz][i]);
+                cout<<matrix[i][zn];
+            }
+            cout<<"   ";
             
-//             // cout<<j;
-//         }
+        }
         
     }
 };
