8f295a10d917c2d54aa5e8e0430d0dad4320491a
Mudar Hussain <v448474@gmail.com>
2022-03-29 18:28:11 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (55.88%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -37,16 +37,29 @@ public:
 //     }
     
     //4th approach
+//     int uniquePaths(int m, int n) {
+//         if(n == 1 || m == 1) return 1;
+//         vector<vector<int>> dp(m,vector<int>(n,1));
+//         for(int i =1; i<m; i++){
+//             for(int j = 1; j<n; j++){
+//                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
+//             }
+//         }
+        
+//         return dp[m-1][n-1];
+//     }
+    
+    //5th Approach
     int uniquePaths(int m, int n) {
         if(n == 1 || m == 1) return 1;
-        vector<vector<int>> dp(m,vector<int>(n,1));
-        for(int i =1; i<m; i++){
+        vector<int> dp(n,1);
+        for(int i = 0; i<m-1; i++){
             for(int j = 1; j<n; j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                dp[j] += dp[j-1];
             }
         }
         
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
     
 };
