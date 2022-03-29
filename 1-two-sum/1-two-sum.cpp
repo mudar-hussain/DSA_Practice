dc790832d1ad6722ff934634dffde70876dc66ec
Mudar Hussain <v448474@gmail.com>
2022-03-30 00:03:34 +0530

    Time: 11 ms (88.14%), Space: 11 MB (28.87%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,18 +1,37 @@
 class Solution {
 public:
+    //2nd approach
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans;
-        int t = target;
+        unordered_map<int,int> um;
         for(int i = 0; i<nums.size(); i++){
-            t -= nums[i];
-            auto it = find(nums.begin()+i+1,nums.end(), t);
-            if(it != nums.end()){
+            int t = target - nums[i];
+            auto it = um.find(t);
+            if(it != um.end()){
                 ans.push_back(i);
-                ans.push_back(it - nums.begin());
-                break;
+                ans.push_back(it->second);
+                return ans;
             }
-            t = target;
+            um[nums[i]] = i;
         }
         return ans;
     }
+    
+    //1st Approach
+//     vector<int> twoSum(vector<int>& nums, int target) {
+    
+//         vector<int> ans;
+//         int t = target;
+//         for(int i = 0; i<nums.size(); i++){
+//             t -= nums[i];
+//             auto it = find(nums.begin()+i+1,nums.end(), t);
+//             if(it != nums.end()){
+//                 ans.push_back(i);
+//                 ans.push_back(it - nums.begin());
+//                 break;
+//             }
+//             t = target;
+//         }
+//         return ans;
+//     }
 };
