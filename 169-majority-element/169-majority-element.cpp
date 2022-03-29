68e3c26203b514f71d402bcfbe87e13a6eb1dcf5
Mudar Hussain <v448474@gmail.com>
2022-03-29 09:39:03 +0530

    Time: 27 ms (49.56%), Space: 19.6 MB (45.87%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return nums[0];
-        vector<int> checked;
-        for(int i = 0; i<n; i++){
-            if(find(checked.begin(), checked.end(), nums[i]) == checked.end()){
-                if(count(nums.begin()+i, nums.end(), nums[i]) > n/2)
-                    return nums[i];
-                checked.push_back(nums[i]);
-            }
-        }
         
-        return -1;
+        int ans = 0;
+        int count = 0;
+        for(int num:nums){
+            if(count == 0) 
+                ans = num;
+            if(num == ans) count++;
+            else count--;
+        }
+        return ans;
+        
+//         if(n==1) return nums[0];
+//         vector<int> checked;
+//         for(int i = 0; i<n; i++){
+//             if(find(checked.begin(), checked.end(), nums[i]) == checked.end()){
+//                 if(count(nums.begin()+i, nums.end(), nums[i]) > n/2)
+//                     return nums[i];
+//                 checked.push_back(nums[i]);
+//             }
+//         }
+        
+//         return -1;
     }
 };
