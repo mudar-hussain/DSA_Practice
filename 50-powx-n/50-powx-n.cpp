5c89ad78482ae7f793088026603e77cc6fcc8d79
Mudar Hussain <v448474@gmail.com>
2022-03-29 08:24:10 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (81.29%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
+    double solve(double x, long n){
+        if(n==0) return 1;
+        if(n<0) return solve(1/x, -1*(n));
+        
+        return n%2? x*solve(x*x, n/2): solve(x*x, n/2);
+    }
     double myPow(double x, int n) {
-        double num = n < 0 ? 1 / x : x;
-        double product{1};
-        n = abs(n);
         
-        while(n){
-            if(n & 1)
-                product *= num;
-            
-            num *= num;
-            
-            n /= 2;
-        }
+        return solve( x, 1l * n);
+//         double num = n < 0 ? 1 / x : x;
+//         double product{1};
+//         n = abs(n);
         
-        return product;    
+//         while(n){
+//             if(n & 1)
+//                 product *= num;
+            
+//             num *= num;
+            
+//             n /= 2;
+//         }
+        
+//         return product;    
     
     }
 };
