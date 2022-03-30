b7dadbf80ab03d30c92b292b7e4bcb58fea99244
Mudar Hussain <v448474@gmail.com>
2022-03-30 12:31:32 +0530

    Time: 424 ms (33.92%), Space: 31 MB (43.30%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
+    //2nd Approach
     int longestConsecutive(vector<int>& nums) {
         int n = nums.size();
         if(n==0) return 0;
-        sort(nums.begin(), nums.end());
-        int i = 0, j = 1;
-        int ans = 1,prev = nums[0],curr = 1;
-        while(j<n){
-            if(nums[j] == prev+1){
-                curr++;
+        unordered_set<int> mp;
+        for(auto i: nums)
+            mp.insert(i);
+        int ans = 1,curr,prev;
+        for(int i = 0; i<n; i++){
+            curr = 1;
+            if(mp.find(nums[i]-1) == mp.end()){
+                prev = nums[i]+1;
+                while(mp.find(prev) != mp.end()){
+                    curr++;
+                    prev++;
+                }
+                ans = max(ans,curr);
             }
-            else if(nums[j] != prev){
-                curr = 1;
-            }
-            prev = nums[j];
-            ans = max(ans, curr);
-            
-            j++;
         }
         return ans;
     }
+    //1st Approach
+//     int longestConsecutive(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n==0) return 0;
+//         sort(nums.begin(), nums.end());
+//         int i = 1;
+//         int ans = 1,prev = nums[0],curr = 1;
+//         while(i<n){
+//             if(nums[i] == prev+1){
+//                 curr++;
+//             }
+//             else if(nums[i] != prev){
+//                 curr = 1;
+//             }
+//             prev = nums[i];
+//             ans = max(ans, curr);
+            
+//             i++;
+//         }
+//         return ans;
+//     }
 };
