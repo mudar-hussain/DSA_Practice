46636f23fade4f5de6c7b2c4b72c84f64c676b52
Mudar Hussain <v448474@gmail.com>
2022-03-31 20:12:40 +0530

    Time: 46 ms (81.79%), Space: 14.6 MB (32.87%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,35 +8,46 @@
  */
 class Solution {
 public:
-    
+    //2nd Approach
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* fast, *slow;
-        ListNode* cal = headA;
-        int l1 = 0, l2 = 0;
-        while(cal){
-            cal = cal->next;
-            l1++;
+        ListNode *l1 = headA, *l2 = headB;
+        while(l1 != l2){
+            // if(l1 == l2) return l1;
+            l1 = !l1? headB:l1->next;
+            l2 = !l2? headA:l2->next;
         }
-        cal = headB;
-        while(cal){
-            cal = cal->next;
-            l2++;
-        }
-        int diff = abs(l1-l2);
-        if(l1>l2) {
-            fast = headA;
-            slow = headB;
-        }else{
-            fast = headB;
-            slow = headA;
-        }
-        while(diff--) fast = fast->next;
-        while(fast  && slow){
-            if(fast==slow) return fast;
-            fast = fast->next;
-            slow = slow->next;
-            
-        }
-        return NULL;
+        
+        return l1;
     }
+    //1st Approach
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode* fast, *slow;
+//         ListNode* cal = headA;
+//         int l1 = 0, l2 = 0;
+//         while(cal){
+//             cal = cal->next;
+//             l1++;
+//         }
+//         cal = headB;
+//         while(cal){
+//             cal = cal->next;
+//             l2++;
+//         }
+//         int diff = abs(l1-l2);
+//         if(l1>l2) {
+//             fast = headA;
+//             slow = headB;
+//         }else{
+//             fast = headB;
+//             slow = headA;
+//         }
+//         while(diff--) fast = fast->next;
+//         while(fast  && slow){
+//             if(fast==slow) return fast;
+//             fast = fast->next;
+//             slow = slow->next;
+            
+//         }
+//         return NULL;
+//     }
 };
