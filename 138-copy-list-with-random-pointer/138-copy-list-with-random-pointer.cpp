e831fd79d391e26a123d1aff2c255cd59eb44123
Mudar Hussain <v448474@gmail.com>
2022-04-02 22:10:32 +0530

    Time: 23 ms (6.35%), Space: 11.2 MB (63.10%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -16,39 +16,60 @@ public:
 
 class Solution {
 public:
+    //2nd Approach
     Node* copyRandomList(Node* head) {
         if(!head) return head;
-        if(!head->next) {
-            Node *a = new Node(head->val);
-            if(head->random) a->random = a;
-            return a;
-        }
         
         unordered_map<Node*, Node*> um;
         Node *temp = head;
-        Node *ans = new Node(-1);
-        Node *tans = ans;
         while(temp){
-            Node *t = new Node(temp->val);
-            tans->next = t;
-            tans = t;
-            um[temp] = tans;
+            um[temp] = new Node(temp->val);
             temp = temp->next;
         }
         temp = head;
-        tans = ans->next;
         while(temp){
-            if(temp->random){
-                tans->random = um[temp->random];
-            }
+            um[temp]->next = um[temp->next];
+            um[temp]->random = um[temp->random];
             temp = temp->next;
-            tans = tans->next;
         }
-        return ans->next;
+        return um[head];
+        
+    }
+    
+    //1st Approach
+//     Node* copyRandomList(Node* head) {
+//         if(!head) return head;
+//         if(!head->next) {
+//             Node *a = new Node(head->val);
+//             if(head->random) a->random = a;
+//             return a;
+//         }
+        
+//         unordered_map<Node*, Node*> um;
+//         Node *temp = head;
+//         Node *ans = new Node(-1);
+//         Node *tans = ans;
+//         while(temp){
+//             Node *t = new Node(temp->val);
+//             tans->next = t;
+//             tans = t;
+//             um[temp] = tans;
+//             temp = temp->next;
+//         }
+//         temp = head;
+//         tans = ans->next;
+//         while(temp){
+//             if(temp->random){
+//                 tans->random = um[temp->random];
+//             }
+//             temp = temp->next;
+//             tans = tans->next;
+//         }
+//         return ans->next;
             
             
         
-    }
+//     }
 };
 
 
