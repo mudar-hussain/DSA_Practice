d1c345c0db0aeb7edfabdfe5367899b08a0348e4
Mudar Hussain <v448474@gmail.com>
2022-04-05 23:11:47 +0530

    Time: 25 ms (34.46%), Space: 15.6 MB (38.16%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    void solve(vector<int> nums,int i, int target, vector<int> curr, vector<vector<int>>& ans){
+    //2nd Approach
+    void solve(vector<int> nums,int ind, int target, vector<int> curr, vector<vector<int>>& ans){
         
         if(target==0) {
-            if(find(ans.begin(), ans.end(), curr) == ans.end())
-                ans.push_back(curr);
+            ans.push_back(curr);
             return;
         }
-        if(i < 0 || target < 0) return ;
-        solve(nums, i-1, target, curr, ans);
-        if(nums[i]<=target){
+        
+        for(int i = ind; i<nums.size() && target>=nums[i]; i++){
+            if(i>ind && nums[i] == nums[i-1]) continue;
             curr.push_back(nums[i]);
             solve(nums, i, target-nums[i], curr, ans);
             curr.pop_back();
@@ -19,9 +19,35 @@ public:
         vector<vector<int>> ans;
         vector<int> curr;
         sort(candidates.begin(), candidates.end());
-        int n = candidates.size();
         
-        solve(candidates, n-1, target, curr, ans);
+        solve(candidates, 0, target, curr, ans);
         return ans;
     }
+    
+    
+    //1st Approach
+//     void solve(vector<int> nums,int i, int target, vector<int> curr, vector<vector<int>>& ans){
+        
+//         if(target==0) {
+//             if(find(ans.begin(), ans.end(), curr) == ans.end())
+//                 ans.push_back(curr);
+//             return;
+//         }
+//         if(i < 0 || target < 0) return ;
+//         solve(nums, i-1, target, curr, ans);
+//         if(nums[i]<=target){
+//             curr.push_back(nums[i]);
+//             solve(nums, i, target-nums[i], curr, ans);
+//             curr.pop_back();
+//         }
+//     }
+//     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+//         vector<vector<int>> ans;
+//         vector<int> curr;
+//         sort(candidates.begin(), candidates.end());
+//         int n = candidates.size();
+        
+//         solve(candidates, n-1, target, curr, ans);
+//         return ans;
+//     }
 };
