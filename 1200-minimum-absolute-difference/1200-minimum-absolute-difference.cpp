520be00a2b57b9a8363f837424629040c56582ef
Mudar Hussain <v448474@gmail.com>
2022-04-10 12:31:29 +0530

    Time: 97 ms (46.61%), Space: 32.2 MB (50.74%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.cpp
@@ -4,13 +4,15 @@ public:
         int n = arr.size();
         vector<vector<int>> ans;
         sort(arr.begin(), arr.end());
-        int mindiff = INT_MAX;
-        for(int i = 0; i<n-1; i++){
-            mindiff = min(mindiff, abs(arr[i]-arr[i+1]));
-        }
-        for(int i = 0; i<n-1; i++){
-            if(abs(arr[i]-arr[i+1]) == mindiff)
-                ans.push_back({arr[i], arr[i+1]});
+        int mindiff = arr[1] - arr[0];
+        
+        for(int i = 1; i<n; i++){
+            if(arr[i]-arr[i-1]<mindiff){
+                ans.clear();
+                mindiff = arr[i]-arr[i-1];
+            }
+            if(arr[i]-arr[i-1] == mindiff)
+                ans.push_back({arr[i-1], arr[i]});
         }
         return ans;
         
