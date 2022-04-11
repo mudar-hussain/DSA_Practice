919c88348695aa4414d779434aadd4ac0a5a2983
Mudar Hussain <v448474@gmail.com>
2022-04-11 09:45:52 +0530

    Time: 8 ms (35.14%), Space: 9.6 MB (5.82%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
+    //2nd Approach
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int low = 0, high = m*n-1;
-        int mid;
-        while(low<=high){
-            mid = low+(high-low)/2;
-            if(target<matrix[mid/m][mid%m]){
-                high = mid-1;
-            }else if(target>matrix[mid/m][mid%m]){
-                low = mid+1;
-            }else if(target == matrix[mid/m][mid%m]){
+        int i = 0, j = m-1;
+        
+        while(i<n && j>=0){
+            if(target==matrix[i][j]){
                 return true;
-            }
-            
-           
+            }else if(target<matrix[i][j]) j--;
+            else i++;
         }
         return false;
     }
+    
+    
+    //1st Approach
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         int n = matrix.size();
+//         int m = matrix[0].size();
+//         int low = 0, high = m*n-1;
+//         int mid;
+//         while(low<=high){
+//             mid = low+(high-low)/2;
+//             if(target<matrix[mid/m][mid%m]){
+//                 high = mid-1;
+//             }else if(target>matrix[mid/m][mid%m]){
+//                 low = mid+1;
+//             }else if(target == matrix[mid/m][mid%m]){
+//                 return true;
+//             }
+            
+           
+//         }
+//         return false;
+//     }
 };
