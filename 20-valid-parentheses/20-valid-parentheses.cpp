0975d8a71be41232714a246f9def80d8d12212cf
Mudar Hussain <v448474@gmail.com>
2022-04-13 11:50:29 +0530

    Time: 4 ms (23.02%), Space: 6.4 MB (10.02%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i<s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]);
+            else{
+                if(st.empty() || (s[i] == ')' && st.top() != '(') || (s[i] == ']' && st.top() != '[') || (s[i] == '}' && st.top() != '{')) return false;
+                st.pop();
+            }
+        }
+        if(st.empty())
+            return true;
+        else return false;
+    }
+};
