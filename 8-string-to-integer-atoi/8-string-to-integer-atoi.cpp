5477a0b64c3c69010cdd1f17f3389be88d13757e
Mudar Hussain <v448474@gmail.com>
2022-04-17 06:31:21 -0400

    Time: 0 ms (100.00%), Space: 7.1 MB (13.07%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -1,25 +1,56 @@
 class Solution {
 public:
+    //2nd Approach
     int myAtoi(string s) {
-        int n = s.length();
-        int i = 0;
+        if(s.length()==0) return 0;
+
+        int i=0;
+         while(i<s.size() && s[i]== ' ') {
+             i++;
+         }     
+        s = s.substr(i); //i ---> last of string
+        
+        int sign = +1;
         long ans = 0;
-        bool neg = false;
-        while(s[i] == ' ') i++;
-        if(s[i] == '-') {
-            neg = true;
-            i++;
-        }else if(s[i] == '+') i++;
-        while(i<n){
-            if(s[i] < '0' || s[i] > '9') break;
-            ans = ans*10 + (s[i] - '0');
+        
+        if(s[0] == '-') sign = -1;
+        
+        int MAX = INT_MAX, MIN = INT_MIN;
+        i = (s[0] == '+' || s[0] == '-') ? 1 : 0;
+		
+        while(i < s.length()) {
+            if(s[0] == ' ' || !isdigit(s[i])) break;
             
-            if(neg && (-1)*ans<=INT_MIN) return INT_MIN;
-            if(!neg && ans>=INT_MAX) return INT_MAX;
-            i++;
+            ans = ans * 10 + s[i]-'0';
+            if(sign == -1 && -1*ans < MIN) return MIN;
+            if(sign == 1 && ans > MAX) return MAX;
             
+            i++;
         }
-        if(neg) return (int)(-1)*ans;
-        return (int)ans;
+        
+        return (int)(sign*ans);
     }
+    //1st Approach
+//     int myAtoi(string s) {
+//         int n = s.length();
+//         int i = 0;
+//         long ans = 0;
+//         bool neg = false;
+//         while(s[i] == ' ') i++;
+//         if(s[i] == '-') {
+//             neg = true;
+//             i++;
+//         }else if(s[i] == '+') i++;
+//         while(i<n){
+//             if(s[i] < '0' || s[i] > '9') break;
+//             ans = ans*10 + (s[i] - '0');
+            
+//             if(neg && (-1)*ans<=INT_MIN) return INT_MIN;
+//             if(!neg && ans>=INT_MAX) return INT_MAX;
+//             i++;
+            
+//         }
+//         if(neg) return (int)(-1)*ans;
+//         return (int)ans;
+//     }
 };
