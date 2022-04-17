a00619d700737ef509fa6347d61de9f39a421017
Mudar Hussain <v448474@gmail.com>
2022-04-17 05:18:16 -0400

    Time: 16 ms (52.81%), Space: 8.1 MB (39.37%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.length();
+        unordered_map<char, int> um;
+        
+        um['I'] = 1;
+        um['V'] = 5;
+        um['X'] = 10;
+        um['L'] = 50;
+        um['C'] = 100;
+        um['D'] = 500;
+        um['M'] = 1000;
+        
+        int ans = um[s[n-1]];
+        for(int i = n-2; i>=0; i--){
+            if(um[s[i]]>=um[s[i+1]]) ans += um[s[i]];
+            else ans -= um[s[i]];
+        }
+        return ans;
+    }
+};
