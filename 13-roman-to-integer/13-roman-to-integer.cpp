edda529f30c335c732b4323d813215a6daab8aad
Mudar Hussain <v448474@gmail.com>
2022-04-17 05:25:53 -0400

    Time: 17 ms (46.81%), Space: 7.7 MB (54.32%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int romanToInt(string s) {
         int n = s.length();
-        unordered_map<char, int> um;
-        
-        um['I'] = 1;
-        um['V'] = 5;
-        um['X'] = 10;
-        um['L'] = 50;
-        um['C'] = 100;
-        um['D'] = 500;
-        um['M'] = 1000;
+        unordered_map<char, int> um= {
+    {'I', 1},
+    {'V', 5},
+    {'X', 10},
+    {'L', 50},
+    {'C', 100},
+    {'D', 500},
+    {'M', 1000}
+};
         
         int ans = um[s[n-1]];
         int curr, prev = ans;
