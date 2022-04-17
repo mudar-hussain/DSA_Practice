ac52d2aff890a8387ade5209172f02481a23040c
Mudar Hussain <v448474@gmail.com>
2022-04-17 01:38:36 -0400

    Time: 4 ms (81.11%), Space: 7.3 MB (67.12%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
+    //2nd Approach ((Time: O(1); Space: O(1)))
     string reverseWords(string s) {
         int n = s.length();
-        vector<string> v;
-        for(int i = 0; i<n; ){
+        if(n==1) return s;
+        string str = "";
+        for(int i = n-1; i>=0; i--){
+            if(s[i] == ' ') continue;
             int j = i;
-            while(j<n && s[j] != ' '){
-                j++;
+            while(j>=0 && s[j] != ' '){
+                j--;
             }
-            if(j!=i) 
-                v.push_back(s.substr(i, j-i));
-            if(j == n-1) break;
-            i = j+1;
-        }
-        s = "";
-        for(int i = v.size()-1; i>=0; i--){
-            s += v[i];
-            if(i!=0) s += " ";
+            j++;
+            s += s.substr(j, i-j+1) + " ";
+            i = j;
         }
         
-        return s;
+        s.pop_back();
+        
+        return s.substr(n,s.length()-1);
     }
+    
+    
+    //1st Approach(Time: O(1); Space: O(n))
+//     string reverseWords(string s) {
+//         int n = s.length();
+//         vector<string> v;
+//         for(int i = 0; i<n; ){
+//             int j = i;
+//             while(j<n && s[j] != ' '){
+//                 j++;
+//             }
+//             if(j!=i) 
+//                 v.push_back(s.substr(i, j-i));
+//             if(j == n-1) break;
+//             i = j+1;
+//         }
+//         s = "";
+//         for(int i = v.size()-1; i>=0; i--){
+//             s += v[i];
+//             if(i!=0) s += " ";
+//         }
+        
+//         return s;
+//     }
 };
