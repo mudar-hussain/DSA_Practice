b6e12089fe8a492b8249b6f027b9c4761ad8d1fd
Mudar Hussain <v448474@gmail.com>
2022-04-20 06:31:16 -0400

    Time: 6 ms (50.98%), Space: 12 MB (98.78%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -11,21 +11,47 @@
  */
 class Solution {
 public:
-    vector<string> ans;
-    void rootToLeaf(TreeNode *root, string s){
-        if(!root) return;
-        s.append("->"+to_string(root->val));
-        if(!root->left && !root->right) {
-            s.erase(s.begin()+0,s.begin()+2);
-            ans.push_back(s);
-            return;
-        }
-        rootToLeaf(root->left,s);
-        rootToLeaf(root->right,s);
-    }
+    //2nd Approach
     vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        if(!root) return ans;
         string s = "";
-        rootToLeaf(root,s);
+        queue<pair<TreeNode*, string>> q;
+        q.push({root, to_string(root->val)});
+        q.push({NULL,""});
+        while(!q.empty()){
+            auto curr = q.front();
+            q.pop();
+            TreeNode *temp = curr.first;
+            string ss = curr.second;
+            if(temp != NULL){
+                if(!temp->left && !temp->right){
+                    ans.push_back(ss);
+                }else{
+                    if(temp->left) q.push({temp->left, ss + "->" + to_string(temp->left->val)});
+                    if(temp->right) q.push({temp->right, ss + "->" + to_string(temp->right->val)});
+                }
+            }else if(!q.empty()) q.push({NULL,""});
+        }
+        
         return ans;
     }
+    //1st Approach
+    // vector<string> ans;
+    // void rootToLeaf(TreeNode *root, string s){
+    //     if(!root) return;
+    //     s.append("->"+to_string(root->val));
+    //     if(!root->left && !root->right) {
+    //         s.erase(s.begin()+0,s.begin()+2);
+    //         ans.push_back(s);
+    //         return;
+    //     }
+    //     rootToLeaf(root->left,s);
+    //     rootToLeaf(root->right,s);
+    // }
+    // vector<string> binaryTreePaths(TreeNode* root) {
+    //     string s = "";
+    //     rootToLeaf(root,s);
+    //     return ans;
+    // }
 };
