341e51d1928bd35ec2f9c79bea4b0fa8ad574b97
Mudar Hussain <v448474@gmail.com>
2022-04-21 07:58:28 -0400

    Time: 3 ms (61.91%), Space: 10.2 MB (7.28%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -11,12 +11,40 @@
  */
 class Solution {
 public:
+    //2nd Approach (BFS)
     
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p == nullptr && q == nullptr) return true;
         if(p == nullptr || q == nullptr) return false;
+        queue<TreeNode*> q1;
+        q1.push(p);
+        queue<TreeNode*> q2;
+        q2.push(q);
+        while(!q1.empty() && !q2.empty()){
         
-        return(p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
-        
+            if(q1.front()->val != q2.front()->val) return false;
+            
+            if(q1.front()->left)  q1.push(q1.front()->left);
+            if(q2.front()->left)  q2.push(q2.front()->left);
+            if(q1.size() != q2.size()) return false;
+            
+            if(q1.front()->right) q1.push(q1.front()->right);
+            if(q2.front()->right) q2.push(q2.front()->right);
+            if(q1.size() != q2.size()) return false;
+            
+            q1.pop();
+            q2.pop();
+            
+        }
+        return true;
     }
+
+    //1st Approach (DFS)
+//     bool isSameTree(TreeNode* p, TreeNode* q) {
+//         if(p == nullptr && q == nullptr) return true;
+//         if(p == nullptr || q == nullptr) return false;
+        
+//         return(p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right,q->right));
+        
+//     }
 };
