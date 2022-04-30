a9d02b8b2d8aac5f4618e4837845bf690f8d72fe
Mudar Hussain <v448474@gmail.com>
2022-04-30 13:51:50 -0400

    Time: 54 ms (53.79%), Space: 36.9 MB (64.05%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -11,47 +11,80 @@
  */
 class Solution {
 public:
-    void st(TreeNode *root, stack<TreeNode *> &s, int direction){
-        if(!root) return;
-        s.push(root);
-        if(direction){
-            while(root->left){
-                
-                root = root->left;
-                s.push(root);
-            }
-        }else{
-            while(root->right){
-                root = root->right;
-                s.push(root);
-            }
-        }
+    //2nd Approach
+    // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+//     bool  dfs(TreeNode *root, int k, int cnt){
+//         if(k == 0) return true;
         
+//         if(root == NULL || cnt == 2) return false;
+        
+//         return (findTarget(root->left, k, cnt) || findTarget(root->right, k, cnt) || findTarget(root->left, k-root->val, cnt+1) || findTarget(root->right, k-root->val, cnt+1));
+//     }
+    
+   // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    void inorder(TreeNode *root, vector<int> &v){
+        if(root == NULL) return;
+        inorder(root->left, v);
+        v.push_back(root->val);
+        inorder(root->right, v);
     }
     bool findTarget(TreeNode* root, int k) {
-        if(!root) return false;
-        stack<TreeNode*> mins, maxs;
-        st(root,mins, 1);
-        st(root,maxs, 0);
-        while(!mins.empty() && !maxs.empty() && mins.top()->val < maxs.top()->val){
-            int x = mins.top()->val;
-            int y = maxs.top()->val;
-            if(x+y == k) return true;
-            else if(x+y < k) {
-                TreeNode *temp = mins.top();
-                mins.pop();
-                st(temp->right, mins, 1);
-            }
-            else{
-                TreeNode *temp = maxs.top();
-                maxs.pop();
-                st(temp->left, maxs, 0);
-            }
-             
+        if(root == NULL) return false;
+        vector<int> v;
+        inorder(root, v);
+        int l = 0, r = v.size()-1;
+        while(l<r){
+            if(v[l]+v[r] == k) 
+                return true;
+            else if(v[l]+v[r] < k)
+                l++;
+            else 
+                r--;
         }
         return false;
         
     }
+    //1st Approach
+//     void st(TreeNode *root, stack<TreeNode *> &s, int direction){
+//         if(!root) return;
+//         s.push(root);
+//         if(direction){ 
+//             while(root->left){
+//                 root = root->left;
+//                 s.push(root);
+//             }
+//         }else{
+//             while(root->right){
+//                 root = root->right;
+//                 s.push(root);
+//             }
+//         }
+        
+//     }
+//     bool findTarget(TreeNode* root, int k) {
+//         if(!root) return false;
+//         stack<TreeNode*> mins, maxs;
+//         st(root,mins, 1);
+//         st(root,maxs, 0);
+//         while(!mins.empty() && !maxs.empty() && mins.top()->val < maxs.top()->val){
+//             int x = mins.top()->val;
+//             int y = maxs.top()->val;
+//             if(x+y == k) return true;
+//             else if(x+y < k) {
+//                 TreeNode *temp = mins.top();
+//                 mins.pop();
+//                 st(temp->right, mins, 1);
+//             }
+//             else{
+//                 TreeNode *temp = maxs.top();
+//                 maxs.pop();
+//                 st(temp->left, maxs, 0);
+//             }
+             
+//         }
+//         return false;
+        
+//     }
 };
 
 
