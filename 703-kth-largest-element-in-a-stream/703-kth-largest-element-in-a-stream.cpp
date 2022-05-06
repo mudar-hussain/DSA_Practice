2c6a6529db0a36e97d062e85ec23adca677e284c
Mudar Hussain <v448474@gmail.com>
2022-05-06 12:33:14 -0400

    Time: 40 ms (69.41%), Space: 19.9 MB (18.84%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -1,34 +1,60 @@
+//2nd Approach (1 queue)
+
 class KthLargest {
-    int kk;
+    int k;
     priority_queue<int, vector<int>, greater<int>> pqmin;
-    priority_queue<int> pqmax;
 public:
     KthLargest(int k, vector<int>& nums) {
-        kk = k;
-        if(nums.size()>0){
-            sort(nums.begin(), nums.end(), greater<int>());
-            for(int i = 0; i<nums.size(); i++){
-                if(i<k)
-                    pqmin.push(nums[i]);
-                else 
-                    pqmax.push(nums[i]);
-            }
+        this->k = k;
+        for(auto n:nums){
+            pqmin.push(n);
+            if(pqmin.size()>k)
+                pqmin.pop();
         }
     }
     
     int add(int val) {
-        if(pqmin.size()<kk)
-            pqmin.push(val);
-        else if(val>pqmin.top()){
-            pqmax.push(pqmin.top());
+        pqmin.push(val);
+        if(pqmin.size()>k)
             pqmin.pop();
-            pqmin.push(val);
-        }else{
-            pqmax.push(val);
-        }
         return pqmin.top();
     }
 };
+
+//1st Approach (2 QUEUE)
+
+
+// class KthLargest {
+//     int kk;
+//     priority_queue<int, vector<int>, greater<int>> pqmin;
+//     priority_queue<int> pqmax;
+// public:
+//     KthLargest(int k, vector<int>& nums) {
+//         kk = k;
+//         if(nums.size()>0){
+//             sort(nums.begin(), nums.end(), greater<int>());
+//             for(int i = 0; i<nums.size(); i++){
+//                 if(i<k)
+//                     pqmin.push(nums[i]);
+//                 else 
+//                     pqmax.push(nums[i]);
+//             }
+//         }
+//     }
+    
+//     int add(int val) {
+//         if(pqmin.size()<kk)
+//             pqmin.push(val);
+//         else if(val>pqmin.top()){
+//             pqmax.push(pqmin.top());
+//             pqmin.pop();
+//             pqmin.push(val);
+//         }else{
+//             pqmax.push(val);
+//         }
+//         return pqmin.top();
+//     }
+// };
 
 /**
  * Your KthLargest object will be instantiated and called as such:
