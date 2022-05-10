b9dca4c3aef36b8edc1cf90e3d8872acb6911932
Mudar Hussain <v448474@gmail.com>
2022-05-10 02:32:22 -0400

    Time: 19 ms (91.17%), Space: 14.1 MB (47.49%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,37 +1,80 @@
+//2nd Approach (BFS)
+
 class Solution {
-    
-    bool isCyclic(vector<vector<int>> &adj, vector<int> &vis, int curr){
-        if(vis[curr] == 2) return true;
-        
-        vis[curr] = 2;
-        for(int i: adj[curr]){
-            if(vis[i] != 1){
-                if(isCyclic(adj, vis, i))
-                    return true;
-            }
-        }
-        
-        vis[curr] = 1;
-        return false;
-    }
-    
 public:
     
     bool canFinish(int n, vector<vector<int>>& p) {
-        vector<int> vis(n, 0);
         vector<vector<int>> adj(n);
+        vector<int> inDeg(n,0);
         for(auto i: p){
-            adj[i[0]].push_back(i[1]);
-            // adj[i[1]].push_back(i[0]);
+            adj[i[1]].push_back(i[0]);
+            inDeg[i[0]]++;
         }
         
+        int ans = 0;
+        queue<int> q;
+        
         for(int i = 0; i<n; i++){
-            if(vis[i] == 0){
-                if(isCyclic(adj, vis, i))
-                    return false;
+            if(inDeg[i] == 0){
+                q.push(i);
             }
         }
         
-        return true;
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            ans++;
+            
+            for(int i: adj[curr]){
+                inDeg[i]--;
+                if(inDeg[i] == 0)
+                    q.push(i);
+            }
+        }
+        
+        
+        
+        return ans == n;
     }
 };
+
+
+//1st Approach (Topological Sort - DFS)
+
+// class Solution {
+    
+//     bool isCyclic(vector<vector<int>> &adj, vector<int> &vis, int curr){
+//         if(vis[curr] == 2) return true;
+        
+//         vis[curr] = 2;
+//         for(int i: adj[curr]){
+//             if(vis[i] != 1){
+//                 if(isCyclic(adj, vis, i))
+//                     return true;
+//             }
+//         }
+        
+//         vis[curr] = 1;
+//         return false;
+//     }
+    
+// public:
+    
+//     bool canFinish(int n, vector<vector<int>>& p) {
+//         vector<int> vis(n, 0);
+//         vector<vector<int>> adj(n);
+//         for(auto i: p){
+//             adj[i[0]].push_back(i[1]);
+//             // adj[i[1]].push_back(i[0]);
+//         }
+        
+//         for(int i = 0; i<n; i++){
+//             if(vis[i] == 0){
+//                 if(isCyclic(adj, vis, i))
+//                     return false;
+//             }
+//         }
+        
+//         return true;
+//     }
+// };
