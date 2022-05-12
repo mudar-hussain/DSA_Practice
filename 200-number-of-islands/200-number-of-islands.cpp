094e06ce35051612e05854d85849cd2a0ad59f87
Mudar Hussain <v448474@gmail.com>
2022-05-12 14:10:09 -0400

    Time: 73 ms (17.26%), Space: 12.2 MB (97.53%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    void dp(vector<vector<char>>& grid, vector<vector<bool>>& vis, int i, int j){
-        if(i<0 || j<0 || i==grid.size() || j==grid[0].size() || vis[i][j] == true || grid[i][j] == '0')
+    void dp(vector<vector<char>>& grid, int i, int j){
+        if(i<0 || j<0 || i==grid.size() || j==grid[0].size()  || grid[i][j] == '0')
             return;
-        vis[i][j] = true;
-        dp(grid, vis, i-1, j);
-        dp(grid, vis, i, j-1);
-        dp(grid, vis, i+1, j);
-        dp(grid, vis, i, j+1);
+        grid[i][j] = '0';
+        dp(grid, i-1, j);
+        dp(grid, i, j-1);
+        dp(grid, i+1, j);
+        dp(grid, i, j+1);
     }
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         int ans = 0;
-        vector<vector<bool>> vis(m, vector<bool>(n,false));
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                if(grid[i][j] == '1' && vis[i][j] == false){
+                if(grid[i][j] == '1'){
                     ans++;
-                    dp(grid, vis, i, j);
+                    dp(grid, i, j);
                 }
                     
             }
