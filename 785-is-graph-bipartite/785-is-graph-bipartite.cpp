c45675b027383a0ca48690ae3a0c943378646dd7
Mudar Hussain <v448474@gmail.com>
2022-05-13 08:46:52 -0400

    Time: 41 ms (25.20%), Space: 13.5 MB (79.98%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,37 +1,64 @@
 class Solution {
 public:
-    //2nd Approach (Graph Coloring BFS)
-    bool isB(vector<vector<int>> &graph, vector<int> &color, int i){
-        queue<int> q;
-        q.push(i);
-        color[i] = 1;
+    //3rd Approach (Graph Coloring DFS)
+    bool isB(vector<vector<int>> &graph, vector<int> &color, int curr){
+           for(int ele: graph[curr]){
+               if(color[ele] == color[curr])
+                   return false;
+               if(color[ele] == -1){
+                   color[ele] = 1-color[curr];
+                   if(!isB(graph, color, ele))
+                       return false;
+               }
+           }
+        return true;
         
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            for(int ele: graph[curr]){
-                if(color[ele] == color[curr]){
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n = graph.size();
+        if(n == 0 || n == 1) return true;
+        vector<int> color(n+1, -1);
+        for(int i = 0; i<n; i++){
+            if(color[i] == -1){
+                color[i] = 1;
+                if(!isB(graph, color, i))
                     return false;
-                }
-                if(color[ele] == -1){
-                    color[ele] = 1-color[curr];
-                    q.push(ele);
-                }
             }
         }
         return true;
+        
     }
-    bool isBipartite(vector<vector<int>>& graph) {
-       int n = graph.size();
-        if(n==0 || n==1) return true;
-        vector<int> color(n+1, -1);
-        for(int i = 0; i<n; i++){
-            if(color[i] == -1)
-                if(!isB(graph, color, i))
-                    return false;
-        }
-        return true;
-    }
+
+    //2nd Approach (Graph Coloring BFS)
+    // bool isB(vector<vector<int>> &graph, vector<int> &color, int curr){
+    //     queue<int> q;
+    //     q.push(0);
+    //     color[curr] = 1;
+    //     while(!q.empty()){
+    //         int curr = q.front();
+    //         q.pop();
+    //         for(int ele: graph[curr]){
+    //             if(color[ele] == color[curr])
+    //                 return false;
+    //             if(color[ele] == -1){
+    //                 q.push(ele);
+    //                 color[ele] = 1-color[curr];
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    // bool isBipartite(vector<vector<int>>& graph) {
+    //    int n = graph.size();
+    //     if(n==0 || n==1) return true;
+    //     vector<int> color(n+1, -1);
+    //     for(int i = 0; i<n; i++){
+    //         if(color[i] == -1)
+    //             if(!isB(graph, color, i))
+    //                 return false;
+    //     }
+    //     return true;
+    // }
     
     //1st Approach (SET)
 //     bool isBipartite(vector<vector<int>>& graph) {
