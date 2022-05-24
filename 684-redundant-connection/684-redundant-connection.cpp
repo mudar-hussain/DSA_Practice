f7497f2739f9b46b9fcf588595e0910d654bf4f9
Mudar Hussain <v448474@gmail.com>
2022-05-24 03:44:36 -0400

    Time: 8 ms (75.17%), Space: 9.1 MB (47.52%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
-    int find(int i, vector<int> &p){
-        if(p[i] == -1)
+    //Approach 2 (Disjoint Optimised)
+    int find(int i, vector<int> &parent){
+        if(parent[i] == -1)
             return i;
-        return find(p[i], p);
+        return find(parent[i], parent);
+            
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        vector<int> parent(n+1, -1);
+        vector<int> parent(n+1,-1);
+        vector<int> rank(n+1, 0);
         for(auto i: edges){
             int p1 = find(i[0], parent);
             int p2 = find(i[1], parent);
             if(p1 == p2)
                 return i;
-            parent[p1] = p2;
+            if(rank[p1] < rank[p2])
+                parent[p1] = p2;
+            else if(rank[p1] > rank[p2])
+                parent[p2] = p1;
+            else{
+                parent[p1] = p2;
+                rank[p2]++;
+            }
         }
         return {};
+        
     }
+    
+    //Approach 1 (Disjoint)
+    // int find(int i, vector<int> &p){
+    //     if(p[i] == -1)
+    //         return i;
+    //     return find(p[i], p);
+    // }
+    // vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+    //     int n = edges.size();
+    //     vector<int> parent(n+1, -1);
+    //     for(auto i: edges){
+    //         int p1 = find(i[0], parent);
+    //         int p2 = find(i[1], parent);
+    //         if(p1 == p2)
+    //             return i;
+    //         parent[p1] = p2;
+    //     }
+    //     return {};
+    // }
 };
