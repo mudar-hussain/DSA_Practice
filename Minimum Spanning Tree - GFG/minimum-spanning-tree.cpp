bd348ad6e37b59ced6f5dc83f6b70dab1df6bc12
Mudar Hussain <v448474@gmail.com>
2022-05-26 02:06:54 -0400

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -9,22 +9,86 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
+	
+	// Krushkal Algo
+	int find(int i, vector<int> parent){
+	    if(parent[i] == i)
+	        return i;
+	   
+	   return find(parent[i], parent);
+	}
+	
+	void unionn(int pu, int pv, vector<int> &parent, vector<int> &rank){
+	    if(rank[pu] < rank[pv])
+	        parent[pu] = pv;
+	   else if(rank[pu] > rank[pv])
+	        parent[pv] = pu;
+	   else {
+	       parent[pu] = pv;
+	       rank[pv]++;
+	   }
+	}
+	
+	int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        vector<int> vis(V,0);
-       priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
-       int sum=0;
-       q.push({0,0});
-       while(!q.empty()){
-           auto b=q.top();q.pop();
-           if(vis[b.second]) continue;
-           sum+=b.first;
-           vis[b.second]++;
-           for(auto i:adj[b.second]) q.push({i[1],i[0]});
-       }
-       return sum;
+        int ans = 0;
+        
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        vector<int> parent(V);
+        for(int i = 0; i<V; i++){
+            parent[i] = i;
+            for(auto x: adj[i]){
+                pq.push({x[1], {i, x[0]}});
+            }
+        }
+        vector<int> rank(V, 0);
+        
+        while(!pq.empty()){
+            auto cur = pq.top();
+            pq.pop();
+            int wt = cur.first;
+            int u = cur.second.first;
+            int v = cur.second.second;
+            int pu = find(u, parent);
+            int pv = find(v, parent);
+            if(pu != pv){
+                ans += wt;
+                unionn(pu, pv, parent, rank);
+            }
+        }
+        return ans;
+        
+        
+        
     }
+	
+	//Prism Algo
+	
+    // int spanningTree(int V, vector<vector<int>> adj[])
+    // {
+    //     // code here
+        
+    //     int ans = 0;
+        
+    //     vector<int> vis(V, 0);
+    //     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+    //     q.push({0, 0});
+        
+    //     while(!q.empty()){
+    //         auto cur = q.top();
+    //         q.pop();
+    //         if(vis[cur.second]) 
+    //             continue;
+    //         ans += cur.first;
+            
+    //         vis[cur.second]++;
+    //         for(auto i: adj[cur.second]){
+    //             q.push({i[1], i[0]});
+    //         }
+    //     }
+    //     return ans;
+    // }
 };
 
 // { Driver Code Starts.
