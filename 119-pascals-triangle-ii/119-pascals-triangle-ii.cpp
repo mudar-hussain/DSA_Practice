5552fb400c833d9ac2c9556b909bd59bbb966065
Mudar Hussain <v448474@gmail.com>
2022-05-28 12:30:54 -0400

    Time: 0 ms (100.00%), Space: 6.5 MB (52.26%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> ans;
-        ans.push_back(1);
-        
-        if(rowIndex==0)return ans;
-        
-        unsigned long long base = rowIndex;
-        ans.push_back(rowIndex);
-        
-        for(int i = 1; i<rowIndex; i++){
-            unsigned long long z  = (ans[i]*(base-i))/(i+1);
-            ans.push_back(z);
+        long k = 1;
+        for(int i = 0; i<=rowIndex; i++){
+            ans.push_back(k);
+            k  = k* (rowIndex-i) / (i+1);
+            
         }
         return ans;
     }
