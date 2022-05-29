8febe77404af603cf490dbb24e2353b8dd400dc6
Mudar Hussain <v448474@gmail.com>
2022-05-29 14:23:42 -0400

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,29 +9,69 @@ using namespace std;
 
 class Solution{   
 public:
-    bool isSubsetSum(vector<int>arr, int sum){
-        // code here 
-        int n = arr.size();
-        int dp[n+1][sum+1];
-        for(int i = 0; i<=n; i++){
-            for(int j = 0; j<=sum; j++){
-                if(i == 0 || j == 0){
-                    if(i == 0)
-                        dp[i][j] = false;
-                    if(j == 0)
-                        dp[i][j] = true;
-                }else{
-                    if(arr[i-1] <= j){
-                        dp[i][j] = (dp[i-1][j-arr[i-1]] || dp[i-1][j]);
-                    }
-                    else{
-                        dp[i][j] = dp[i-1][j];
-                    }
-                }
-            }
+//2nd Approach
+    bool solve(vector<int> &arr, vector<vector<int>> &dp, int n, int sum){
+        if(sum == 0){
+            return true;
         }
-        return dp[n][sum];
+        if(n == -1)
+            return false;
+        
+        if(dp[n][sum] != -1)
+            return dp[n][sum];
+            
+        if(arr[n] <= sum)
+        return dp[n][sum] = (solve(arr, dp, n-1, sum-arr[n]) || solve(arr, dp, n-1, sum));
+        
+        return dp[n][sum] = solve(arr, dp, n-1, sum);
     }
+    bool isSubsetSum(vector<int>arr, int sum){
+        int n = arr.size();
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, -1));
+        return solve(arr, dp, n-1, sum);
+    }
+    
+    //2nd Approach
+    // bool dp(vector<int> &arr, int n, int sum){
+    //     if(sum == 0){
+    //         return true;
+    //     }
+    //     if(n == -1)
+    //         return false;
+            
+    //     return (dp(arr, n-1, sum-arr[n]) || dp(arr, n-1, sum));
+    // }
+    // bool isSubsetSum(vector<int>arr, int sum){
+    //     int n = arr.size();
+        
+    //     return dp(arr, n-1, sum);
+    // }
+    
+    
+//1st Approach
+    // bool isSubsetSum(vector<int>arr, int sum){
+    //     // code here 
+    //     int n = arr.size();
+    //     int dp[n+1][sum+1];
+    //     for(int i = 0; i<=n; i++){
+    //         for(int j = 0; j<=sum; j++){
+    //             if(i == 0 || j == 0){
+    //                 if(i == 0)
+    //                     dp[i][j] = false;
+    //                 if(j == 0)
+    //                     dp[i][j] = true;
+    //             }else{
+    //                 if(arr[i-1] <= j){
+    //                     dp[i][j] = (dp[i-1][j-arr[i-1]] || dp[i-1][j]);
+    //                 }
+    //                 else{
+    //                     dp[i][j] = dp[i-1][j];
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     return dp[n][sum];
+    // }
 };
 
 // { Driver Code Starts.
