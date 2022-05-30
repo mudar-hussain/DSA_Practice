5ac936155926cd8b4962814d9235411aa8e1e01a
Mudar Hussain <v448474@gmail.com>
2022-05-30 06:17:57 -0400

    Time: 13 ms (77.86%), Space: 9.1 MB (78.01%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,20 +1,57 @@
 class Solution {
 public:
-    void knapsack(vector<int> &nums, int n, int target, int &ans){
-        if(n == -1 && target == 0){
-            ans++;
-            return;
-        }else if(n == -1){
-            return;
+    //2nd Approach
+    
+    int countSubsets(vector<int>& nums, int target) {
+        int n = nums.size();
+        int dp[n+1][target+1];
+        dp[0][0] = 1;
+        for(int i = 0; i<=n; i++){
+            for(int j = 0; j<=target; j++){
+                if(i == 0 && j == 0)
+                    continue;
+                if(i == 0 && j != 0){
+                    dp[i][j] = 0;
+                }
+                else
+                {
+                    if(nums[i-1] <= j)
+                        dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                    else
+                        dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-        knapsack(nums, n-1, target-nums[n], ans);
-        knapsack(nums, n-1, target+nums[n], ans);
+        return dp[n][target];
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int ans = 0;
-        knapsack(nums, n-1, target, ans);
-        return ans;
+        int sum = 0;
+        for(int i: nums)
+            sum += i;
+        if(abs(target) > sum || (sum+target)%2 != 0)
+            return 0;
+        sum = (sum+target)/2;
+        return countSubsets(nums, sum);
     }
+                               
+                               
+    //1st Approach
+//     void knapsack(vector<int> &nums, int n, int target, int &ans){
+//         if(n == -1 && target == 0){
+//             ans++;
+//             return;
+//         }else if(n == -1){
+//             return;
+//         }
+//         knapsack(nums, n-1, target-nums[n], ans);
+//         knapsack(nums, n-1, target+nums[n], ans);
+//     }
+    
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int ans = 0;
+//         knapsack(nums, n-1, target, ans);
+//         return ans;
+//     }
 };
