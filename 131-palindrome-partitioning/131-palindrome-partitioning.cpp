2cc9579fafe2bdd3165b32fece7a56aa8af14dec
Mudar Hussain <v448474@gmail.com>
2022-06-08 21:21:33 -0400

    Time: 123 ms (86.68%), Space: 49.2 MB (91.01%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    bool isPalindrome(string ss, int s, int e){
-        while(s<e){
-            if(ss.at(s++) != ss.at(e--)) return false;
-            
+    bool isPalindrome(string &s, int i, int j) {
+        while(i < j) {
+            if(s[i++] != s[j--])
+                return false;
         }
         return true;
     }
-    void solve(string s, vector<vector<string>>& ans, vector<string>& ds, int index){
-        if(index == s.length()){
+    
+    void MCM(string &s, vector<vector<string>> &ans, vector<string> &ds, int i, int n){
+        if(i==n){
             ans.push_back(ds);
             return;
         }
-        for(int i = index; i<s.length(); i++){
-            if(isPalindrome(s, index, i)){
-                ds.push_back(s.substr(index,i-index+1));
-                solve(s, ans, ds, i+1);
+        
+        for(int k = i; k<n; k++){
+            if(isPalindrome(s, i, k)){
+                ds.push_back(s.substr(i, k-i+1));
+                MCM(s, ans, ds, k+1, n);
                 ds.pop_back();
             }
         }
@@ -23,7 +25,7 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> ds;
-        solve(s, ans,ds,0);
+        MCM(s, ans, ds, 0, s.length());
         return ans;
     }
 };
