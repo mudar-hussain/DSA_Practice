e798342c6c4487da870c9c62a1849e21acb69a8d
Mudar Hussain <v448474@gmail.com>
2022-06-23 00:20:24 -0400

    Time: 9 ms (16.26%), Space: 15.7 MB (28.38%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -3,42 +3,41 @@ class Solution {
     
 public:
     //Approach 2
-    void backtrack(vector<int> &nums, vector<vector<int>> &ans, vector<int> temp, int s){
-        ans.push_back(temp);
+//     void backtrack(vector<int> &nums, vector<vector<int>> &ans, vector<int> temp, int s){
+//         ans.push_back(temp);
         
-        for(int i = s; i<nums.size(); i++){
-            temp.push_back(nums[i]);
-            backtrack(nums, ans, temp, i+1);
-            temp.pop_back();
-        }
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        backtrack(nums, ans, temp, 0);
-        return ans;
-    }
+//         for(int i = s; i<nums.size(); i++){
+//             temp.push_back(nums[i]);
+//             backtrack(nums, ans, temp, i+1);
+//             temp.pop_back();
+//         }
+//     }
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//         vector<vector<int>> ans;
+//         vector<int> temp;
+//         backtrack(nums, ans, temp, 0);
+//         return ans;
+//     }
     
     
     //Approach 1
-    // void solve(vector<int> in, vector<int> op){
-    //     if(in.size()==0){
-    //         ans.push_back(op);
-    //         return;
-    //     }
-    //     vector<int> op1 = op;
-    //     vector<int> op2 = op;
-    //     op2.push_back(in[0]);
-    //     in.erase(in.begin()+0);
-    //     solve(in,op1);
-    //     solve(in,op2);
-    //     return;
-    // }
-    // vector<vector<int>> subsets(vector<int>& nums) {  
-    //     vector<int> op;
-    //     solve(nums,op);
-    //     return ans;
-    // }
+    void solve(vector<int> &nums, vector<vector<int>> &ans, vector<int> op, int s){
+        if(s == nums.size()){
+            ans.push_back(op);
+            return;
+        }
+        
+        solve(nums, ans, op, s+1);
+        op.push_back(nums[s]);
+        solve(nums, ans, op, s+1);
+        return;
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {  
+        vector<vector<int>> ans;
+        vector<int> op;
+        solve(nums, ans, op, 0);
+        return ans;
+    }
 };
 
     
