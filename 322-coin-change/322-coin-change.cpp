80affa2c11bcc4f60884e0b97dd329d45a309531
Mudar Hussain <v448474@gmail.com>
2022-06-24 01:47:13 -0400

    Time: 124 ms (54.68%), Space: 50.4 MB (7.04%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    //Approach 2
+    int solve(vector<int> &coins, int amt, int i, vector<vector<int>> &dp){
+        if(amt == 0)
+            return dp[i][amt] = 0;
+        if(i == coins.size())
+            return dp[i][amt] = INT_MAX-1;
+        if(dp[i][amt] != -1)
+            return dp[i][amt];
+        
+        int nt = solve(coins, amt, i+1, dp);
+        int t = INT_MAX;
+        
+        if(amt >= coins[i]){
+            t = 1+solve(coins, amt-coins[i], i, dp);
+        }
+        return dp[i][amt] = min(t, nt);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, -1));
+        
+        long long x = solve(coins, amount, 0, dp);
+        if(x == INT_MAX || x == INT_MAX-1)
+            return -1;
+        else return x;
+    }
+    
+    
+    //Appproach 1 (Recursion)
+    
+    // int solve(vector<int> &coins, int amt, int i, int ans){
+    //     if(amt == 0)
+    //         return ans;
+    //     if(i == coins.size())
+    //         return -1;
+    //     if(amt >= coins[i]){
+    //         int a = solve(coins, amt, i+1, ans);
+    //         int b = solve(coins, amt-coins[i], i, ans+1);
+    //         if(a == -1)
+    //             return b;
+    //         else if(b == -1)
+    //             return a;
+    //         else
+    //             return min(a, b);
+    //     }
+    //     else
+    //         return solve(coins, amt, i+1, ans);
+    // }
+    // int coinChange(vector<int>& coins, int amount) {
+    //     return solve(coins, amount, 0, 0);
+    // }
+};
