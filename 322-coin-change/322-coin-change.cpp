80563ecafaf3bf08a668710b19a27310018cb177
Mudar Hussain <v448474@gmail.com>
2022-06-24 02:19:11 -0400

    Time: 86 ms (82.54%), Space: 10.6 MB (84.41%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    
+     
     //Approach 3 (Dynamic Programming)
     
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        int dp[n+1][amount+1];
         for(int i = 0; i<=n; i++){
             dp[i][0] = 0;
         }
@@ -20,11 +22,6 @@ public:
                     dp[i][j] = dp[i-1][j];
             }
         }
-        
-        
-        
-        
-        
         return dp[n][amount]==INT_MAX-1?-1:dp[n][amount];
     }
     
