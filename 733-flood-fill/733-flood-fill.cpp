f8229556a360451988e4937af19621948be68e41
Mudar Hussain <v448474@gmail.com>
2022-06-26 03:58:12 -0400

    Time: 22 ms (16.29%), Space: 14.3 MB (27.12%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -4,31 +4,31 @@ public:
     //2nd Approach (BFS)
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        int m=image.size();
-        int n=image[0].size();
-        int dir[4][2]={{1,0},{0,1},{-1,0},{0,-1}};
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        int oldColor=image[sr][sc];
-        if(image[sr][sc]==newColor){
+        int m = image.size();
+        int n = image[0].size();
+        int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+        int old = image[sr][sc];
+        if(image[sr][sc] == newColor)
             return image;
-        }
-        image[sr][sc]=newColor;
-        
+        image[sr][sc] = newColor;
         while(!q.empty()){
-            int x=q.front().first;
-            int y=q.front().second;
+            int x = q.front().first;
+            int y = q.front().second;
             q.pop();
-            for(int k=0;k<4;k++){
-                int newx=x+dir[k][0];
-                int newy=y+dir[k][1];
-                
-                if(newx>=0 && newy>=0 && newx<m && newy<n && image[newx][newy]==oldColor){
-                    q.push({newx,newy});
-                    image[newx][newy]=newColor;
+            for(int i = 0; i<4; i++){
+                int nx = x+dir[i][0];
+                int ny = y+dir[i][1];
+                if(nx>=0 && ny>=0 && nx<m && ny<n && image[nx][ny] == old){
+                    image[nx][ny] = newColor;
+                    q.push({nx, ny});
                 }
             }
         }
+        
+        
+        
         return image;
     }
     
