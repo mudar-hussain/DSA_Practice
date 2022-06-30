ad35b767922a4b1c5c631b924d9aefa9706e59e4
Mudar Hussain <v448474@gmail.com>
2022-06-30 03:41:42 -0400

    Time: 1118 ms (27.63%), Space: 180.8 MB (41.55%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &adj, vector<vector<int>> &bridges, vector<int> &disc, vector<int> &low, vector<int> &parent, int src){
+    void dfs(vector<vector<int>> &adj, vector<int> &disc, vector<int> &low, vector<int> &parent, vector<vector<int>> &ans, int u){
         static int time = 0;
-        disc[src] = low[src] = time;
+        disc[u] = low[u] = time;
         time++;
-        for(int i: adj[src]){
-            if(i == parent[src]) continue;
-            if(disc[i] == -1){
-                parent[i] = src;
-                dfs(adj, bridges, disc, low, parent, i);
-                low[src] = min(low[src], low[i]);
-                if(low[i] > disc[src])
-                    bridges.push_back({src, i});
-            }else
-                low[src] = min(low[src], disc[i]);
+        for(int v: adj[u]){
+            if(v == parent[u]) continue;
+            if(disc[v] == -1){
+                parent[v] = u;
+                dfs(adj, disc, low, parent, ans, v);
+                low[u] = min(low[u], low[v]);
+                if(low[v] > disc[u])
+                    ans.push_back({u, v});
+            }else 
+                low[u] = min(low[u], disc[v]);
         }
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
         vector<vector<int>> adj(n);
         for(auto i: connections){
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+                adj[i[0]].push_back(i[1]);
+                adj[i[1]].push_back(i[0]);
+            
         }
         vector<int> disc(n, -1);
         vector<int> low(n, -1);
         vector<int> parent(n, -1);
-        vector<vector<int>> bridges;
+        vector<vector<int>> ans;
         for(int i = 0; i<n; i++){
             if(disc[i] == -1)
-                dfs(adj, bridges, disc, low, parent, i);
+                dfs(adj, disc, low, parent, ans, i);
         }
-        return bridges;   
+        return ans;
     }
 };
