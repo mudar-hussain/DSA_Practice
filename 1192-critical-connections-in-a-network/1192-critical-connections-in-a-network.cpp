a981d2e02e3a8fce61b5996f85601f514801bd73
Mudar Hussain <v448474@gmail.com>
2022-06-30 03:45:34 -0400

    Time: 1048 ms (35.08%), Space: 179.3 MB (70.27%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &adj, vector<int> &disc, vector<int> &low, vector<int> &parent, vector<vector<int>> &ans, int u){
+    void dfs(vector<vector<int>> &adj, vector<int> &disc, vector<int> &low, vector<vector<int>> &ans, int u, int parent){
         static int time = 0;
         disc[u] = low[u] = time;
         time++;
         for(int v: adj[u]){
-            if(v == parent[u]) continue;
+            if(v == parent) continue;
             if(disc[v] == -1){
-                parent[v] = u;
-                dfs(adj, disc, low, parent, ans, v);
-                low[u] = min(low[u], low[v]);
+                dfs(adj, disc, low, ans, v, u);
                 if(low[v] > disc[u])
                     ans.push_back({u, v});
-            }else 
-                low[u] = min(low[u], disc[v]);
+            } 
+            low[u] = min(low[u], low[v]);
         }
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
@@ -21,15 +19,13 @@ public:
         for(auto i: connections){
                 adj[i[0]].push_back(i[1]);
                 adj[i[1]].push_back(i[0]);
-            
         }
         vector<int> disc(n, -1);
         vector<int> low(n, -1);
-        vector<int> parent(n, -1);
         vector<vector<int>> ans;
         for(int i = 0; i<n; i++){
             if(disc[i] == -1)
-                dfs(adj, disc, low, parent, ans, i);
+                dfs(adj, disc, low, ans, i, -1);
         }
         return ans;
     }
