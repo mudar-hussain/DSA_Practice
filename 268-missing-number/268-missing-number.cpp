ca563d9fbe367ea0ac7a28e6c040321b93e8ec98
Mudar Hussain <mudar.shussain@gmail.com>
2022-07-02 23:41:01 -0400

    Time: 21 ms (83.04%), Space: 18 MB (18.95%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
+    //Approach 2
     int missingNumber(vector<int>& nums) {
-        int  n  = nums.size();
-        int sum = (n*(n+1))/2;
+        int n = nums.size();
+        int ans = n;
         for(int i = 0; i<n; i++){
-            sum -= nums[i];
+            ans ^= i;
+            ans ^=  nums[i];
+            
         }
+        return ans;
         
-        return sum;
     }
+    //Approach 1
+//     int missingNumber(vector<int>& nums) {
+//         int  n  = nums.size();
+//         int sum = (n*(n+1))/2;
+//         for(int i = 0; i<n; i++){
+//             sum -= nums[i];
+//         }
+        
+//         return sum;
+//     }
 };
