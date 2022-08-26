d3f22f35615a534906801dbf0f3f3e829e3f6d4e
Mudar Hussain <mudar.shussain@gmail.com>
2022-08-26 16:37:01 +0530

    Time: 3 ms (59.15%), Space: 5.8 MB (96.14%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,16 +2,16 @@ class Solution {
     int dp[31];
 public:
     int solve(int n){
-        if(n == 1 || n == 0) return n;
-        if(dp[n] != -1) 
+        if(n == 0 || n == 1)
+            return n;
+        if(dp[n] != -1)
             return dp[n];
         return dp[n] = solve(n-1)+solve(n-2);
     }
     int fib(int n) {
-        if(n == 1 || n == 0) return n;
+        if(n == 0 || n == 1)
+            return n;
         memset(dp, -1, sizeof(dp));
         return solve(n);
-        
-        
     }
 };
