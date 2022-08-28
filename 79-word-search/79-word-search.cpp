054c8b321860d0af51e82287aa2ed65bcd22b42f
Mudar Hussain <mudar.shussain@gmail.com>
2022-08-28 11:18:01 +0530

    Time: 66 ms (99.25%), Space: 8.1 MB (15.91%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int ind, vector<vector<bool>>& vis){
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int ind){
         if(ind>= word.length())
             return true;
         
-        if(i<0 || j<0 || i>=board.size() || j >= board[0].size() || board[i][j] != word[ind] || vis[i][j])
+        if(i<0 || j<0 || i>=board.size() || j >= board[0].size() || board[i][j] != word[ind] || board[i][j] == '*')
             return false;
         
-        vis[i][j] = true;
+        board[i][j] = '*';
         
-        bool down = dfs(board, word, i+1, j, ind+1, vis);
-        bool up = dfs(board, word, i-1, j, ind+1, vis);
-        bool right = dfs(board, word, i, j+1, ind+1, vis);
-        bool left = dfs(board, word, i, j-1, ind+1, vis);
+        bool down = dfs(board, word, i+1, j, ind+1);
+        if(down)    return true;
+        bool up = dfs(board, word, i-1, j, ind+1);
+        if(up)    return true;
+        bool right = dfs(board, word, i, j+1, ind+1);
+        if(right)    return true;
+        bool left = dfs(board, word, i, j-1, ind+1);
+        if(left)    return true;
         
-        vis[i][j] = false;
+        board[i][j] = word[ind];
         
-        return up || down || right || left;
+        return false;
         
         
     }
     bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        
+        int freq[128] = {};
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                freq[board[i][j]]++;
+            }
+        }
+        for(auto ch: word){
+            if(--freq[ch]<0)
+                return false;
+        }
+        
+        // vector<vector<bool>> vis(n, vector<bool>(m, false));
         for(int i = 0; i<n;i++){
             for(int j = 0; j<m; j++){
                 if(board[i][j] == word[0]){
-                    if(dfs(board, word, i, j, 0, vis))
+                    if(dfs(board, word, i, j, 0))
                         return true;
                 }
             }
