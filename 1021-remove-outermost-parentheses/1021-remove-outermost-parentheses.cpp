0e89280db63cefec8de6a8c433f658b38d7b7815
Mudar Hussain <mudar.shussain@gmail.com>
2022-09-02 10:06:39 +0530

    Time: 4 ms (67.21%), Space: 6.7 MB (81.14%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    void solve(string s, int n, string& ans, int idx, int balance){
-        if(idx >= n){
+    void solve(string& s, string& ans, int idx, int balance){
+        if(idx >= s.length()){
             return;
         }
         if(!((balance == 1 && s[idx] == ')') || (balance == 0 && s[idx] == '(')))
@@ -14,13 +14,13 @@ public:
             balance--;
         }
         
-        solve(s, n, ans, idx+1, balance);
+        solve(s, ans, idx+1, balance);
         
     }
     string removeOuterParentheses(string s) {
         string ans = "";
         int n = s.length();
-        solve(s, n, ans, 0, 0);
+        solve(s, ans, 0, 0);
         return ans;
     }
 };
