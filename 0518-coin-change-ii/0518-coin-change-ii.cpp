518c6269f8526e3edce99fdac4350b6221796e98
Mudar Hussain <mudar.shussain@gmail.com>
2022-10-23 12:29:06 +0530

    Time: 112 ms (17.17%), Space: 18.6 MB (31.51%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,76 @@
 class Solution {
 public:
-    int dp[301][5001];
-    int solve(vector<int> &coins, int amt, int i){
-        if(amt<0 || i>=coins.size())
-            return 0;
-        if(amt == 0){
-            return dp[i][amt] = 1;
-        }
+//     int dp[301][5001];
+//     int solve(vector<int> &coins, int amt, int i){
+//         if(amt<0 || i>=coins.size())
+//             return 0;
         
-        if(dp[i][amt] != -1)
-            return dp[i][amt];
+//         if(amt == 0){
+//             return dp[i][amt] = 1;
+//         }
         
-        return dp[i][amt] = solve(coins, amt-coins[i], i)  + solve(coins, amt, i+1);
-    }
+//         if(dp[i][amt] != -1)
+//             return dp[i][amt];
+        
+//         return dp[i][amt] = solve(coins, amt-coins[i], i) + solve(coins, amt, i+1);
+//     }
     
+    
+//     int change(int amount, vector<int>& coins) {
+//         memset(dp, -1, sizeof(dp));
+//         return solve(coins, amount, 0);
+//     }
     
     int change(int amount, vector<int>& coins) {
-        memset(dp, -1, sizeof(dp));
-        return solve(coins, amount, 0);
+        int n = coins.size();
+        if(amount == 0)
+            return 1;
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        for(int i= 0; i<=n; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i<=amount; i++){
+            dp[0][i] = 0;
+        }
+        
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=amount; j++){
+                if(coins[i-1] > j)
+                    dp[i][j] = dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+            }
+        }
+        
+        return dp[n][amount];
+        
     }
+    
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
