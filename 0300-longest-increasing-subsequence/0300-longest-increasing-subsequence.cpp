930452253b3da140499594f3a5ccba26708963ed
Mudar Hussain <mudar.shussain@gmail.com>
2022-10-23 19:45:58 +0530

    Time: 643 ms (36.41%), Space: 10.5 MB (49.86%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    
-    int dp[2502][2502];
-    int solve(vector<int>& nums, int pi, int i){
-        if(i >= nums.size())
-            return 0;
-        
-        if(dp[i][pi+1] != -1)
-            return dp[i][pi+1];
-        
-        int len = solve(nums, pi, i+1);
-        
-        if(pi == -1 || nums[i] > nums[pi])
-            len = max(len, 1+solve(nums, i, i+1));
-        
-        return dp[i][pi+1] = len;
-        
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return solve(nums, -1, 0);
-        
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<i; j++){
+                if(nums[i]>nums[j])
+                    dp[i] = max(dp[i], 1+dp[j]);
+            }
+        }
+        int ans = INT_MIN;
+        for(int i: dp)
+            ans = max(ans, i);
+        return ans;
     }
 };
