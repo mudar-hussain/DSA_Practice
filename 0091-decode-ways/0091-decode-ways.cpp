96fbcf270e2e6b8e20cf22e9b546c7d37c469a45
Mudar Hussain <mudar.shussain@gmail.com>
2022-11-03 09:55:22 +0530

    Time: 0 ms (100.00%), Space: 6 MB (91.30%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
-    int dp[101];
-    int solve(string &s,  int idx){
-        if(idx >= s.length())
-            return 1;
-        if(s[idx] == '0')
-            return 0;
-        
-        if(dp[idx] != -1)
-            return dp[idx];
-        
-        
-        int a = solve(s, idx+1);
-        int b = 0;
-        if(idx < s.length()-1){
-            if(s[idx] == '1' || (s[idx] == '2' && s[idx+1] < '7'))
-                b = solve(s, idx+2);
-        }
-        return dp[idx] = a+b;
-    }
     int numDecodings(string s) {
-        memset(dp, -1, sizeof(dp));
-        return solve(s, 0);
+        int n = s.length();
+        int dp = 0, dp1 = 1, dp2 = 0;
+        for(int i = n-1; i>=0; i--){
+            if(s[i] != '0') //Single Digit
+                dp += dp1;
+            if(i+1 <= n-1 && (s[i] == '1' || (s[i] == '2' && s[i+1] <= '6')))
+                dp += dp2;
+            dp2 = dp1;
+            dp1 = dp;
+            dp = 0;
+        }
+        return dp1;
     }
 };
