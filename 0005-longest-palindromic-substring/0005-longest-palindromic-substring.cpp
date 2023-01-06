4fac1d84597d0e444b565d780d49b466709a37e8
Mudar Hussain <mudar.shussain@gmail.com>
2023-01-06 21:28:08 +0530

    Time: 208 ms (35.79%), Space: 6.8 MB (89.58%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -12,10 +12,9 @@ public:
         int n = s.length();
         string longestPalindrome = "";
         for(int i = 0; i<n && (n-i)>(longestPalindrome.length()); i++){
-            for(int j = n-1; j>=i; j--){
+            for(int j = n-1; j>=i && longestPalindrome.length() < j-i+1; j--){
                 if(isPalindrome(s, i, j)){
-                    if(longestPalindrome.length() < j-i+1)
-                        longestPalindrome = s.substr(i, j-i+1);
+                    longestPalindrome = s.substr(i, j-i+1);
                     break;
                 }
                     
