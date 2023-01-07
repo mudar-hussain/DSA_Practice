41f3dbba8c6eb5d88c9b3ea9ed4a0345a032dacf
Mudar Hussain <mudar.shussain@gmail.com>
2023-01-07 19:47:37 +0530

    Time: 9 ms (33.67%), Space: 7.2 MB (19.68%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-
+    unordered_map<string, string> map;
+    int n = 0;
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        string res = "http://tinyurl.com/" + to_string(n);
+        n++;
+        map[res] = longUrl;
+        return res;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        return map[shortUrl];
     }
 };
 
