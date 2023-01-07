76c94aed8d9145d6d0841afb22f85f8bc6090a49
Mudar Hussain <mudar.shussain@gmail.com>
2023-01-07 10:54:11 +0530

    Time: 11 ms (92.31%), Space: 15.9 MB (45.74%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int i, int j){
-        while(i<j){
-            if(s[i++]!=s[j--])
-                return false;
+    
+    string findPalindrome(string &s, int &n, int i, int j){
+        if(i<0 || j>= n || s[i] != s[j]){
+            return s.substr(i+1, j-1-i);
         }
-        return true;
         
+        return findPalindrome(s, n, i-1, j+1);
     }
+    
+    
     string longestPalindrome(string s) {
+        if(s.length() == 0)
+            return "";
         int n = s.length();
-        string longestPalindrome = "";
-        for(int i = 0; i<n && (n-i)>(longestPalindrome.length()); i++){
-            for(int j = n-1; j>=i && longestPalindrome.length() < j-i+1; j--){
-                if(isPalindrome(s, i, j)){
-                    longestPalindrome = s.substr(i, j-i+1);
-                    break;
+        string ans = s.substr(0,1);
+        int maxi = 1;
+        string temp = "";
+        for(int i = 1; i<s.length(); i++){
+            //even
+            if(s[i] == s[i-1]){
+                temp = findPalindrome(s, n, i-1, i);
+                if(temp.length()>maxi){
+                    ans = temp;
+                    maxi = ans.length();
                 }
-                    
+            }
+            //odd
+            if(i+1<=n-1 && s[i-1] == s[i+1]){
+                temp = findPalindrome(s, n, i-1, i+1);
+                if(temp.length()>maxi){
+                    ans = temp;
+                    maxi = ans.length();
+                }
             }
         }
-        return longestPalindrome;
+        return ans;
+        
     }
 };
