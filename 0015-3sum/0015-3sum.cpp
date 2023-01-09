84b31224b285830eaa8806683480319ef76ee835
Mudar Hussain <mudar.shussain@gmail.com>
2023-01-09 11:44:00 +0530

    Time: 87 ms (86.59%), Space: 20 MB (65.27%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,28 +1,60 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;            // O(3^k) k = no of triplets
-        sort(nums.begin(), nums.end());     // O(NlogN)
+        int n = nums.size();
+        vector<vector<int>> ans;
+        if(n<3)
+            return ans;
+        sort(nums.begin(), nums.end());
+        if(nums[0] > 0)
+            return ans;
         
-        for(int i = 0; i<nums.size()-2; i++){   // O(N^2)
+        for(int i = 0; i<n-2; i++){
+            if(nums[i]>0)
+                break;
             if(i>0 && nums[i] == nums[i-1])
                 continue;
-            int low = i+1, high = nums.size()-1;    
-            while(low<high){                                
-                if(nums[i] + nums[low] + nums[high] == 0){
+            
+            int low = i+1, high = n-1;
+            int sum = 0;
+            while(low<high){
+                sum = nums[i] + nums[low] + nums[high];
+                if(sum<0)
+                    low++;
+                else if(sum>0)
+                    high--;
+                else{
                     ans.push_back({nums[i], nums[low], nums[high]});
                     while(low<high && nums[low] == nums[low+1]) low++;
                     while(low<high && nums[high] == nums[high-1]) high--;
                     low++;
                     high--;
                 }
-                else if(nums[i] + nums[low] + nums[high] < 0)
-                    low++;
-                else
-                    high--;
             }
-            
         }
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
