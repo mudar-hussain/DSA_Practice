c9084ba9a659dd658b5b86e0107a7ca8899e8676
Mudar Hussain <mudar.shussain@gmail.com>
2023-02-08 13:29:49 +0530

    Time: 8 ms (98.04%), Space: 16.5 MB (84.26%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,25 +1,15 @@
 class Solution {
-    int dp[10001];
-    
-    int solve(vector<int>& nums, int idx){
-        if(idx>=nums.size()-1)
-            return 0;
-        if(nums[idx] == 0)
-            return INT_MAX;
-        
-        if(dp[idx] != -1)
-            return dp[idx];
-        
-        int mini = INT_MAX-1;
-        for(int i = 1; i<=nums[idx]; i++){
-            mini = min(mini, solve(nums, idx+i));
-        }
-        return dp[idx] = 1+mini;
-    }
-    
 public:
     int jump(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return solve(nums, 0);
+        int n = nums.size(), cur_end = 0, nxt_end = 0, steps = 0;
+        for(int i = 0; i<n; i++){
+            if(i>cur_end){
+                steps++;
+                cur_end = nxt_end;
+            }
+            
+            nxt_end = max(nxt_end, i+nums[i]);
+        }
+        return steps;
     }
 };
