efcb734ef218b56d8f71e8d1d0e9ce1e7b17d3b4
Mudar Hussain <mudar.shussain@gmail.com>
2023-02-12 11:30:59 +0530

    Time: 11 ms (93.35%), Space: 13.2 MB (90.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -16,12 +16,18 @@ public:
             }
         }
         
+        
         for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(row[i] || col[j]){
+            if(row[i])
+                for(int j = 0; j<m; j++){
                     matrix[i][j] = 0;
                 }
-            }
+        }
+        for(int j = 0; j<m; j++){
+            if(col[j])
+                for(int i = 0; i<n; i++){
+                    matrix[i][j] = 0;
+                }
         }
         
         
