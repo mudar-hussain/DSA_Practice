555c8206cd45d9e43401729dc67907339e74aa90
Mudar Hussain <mudar.shussain@gmail.com>
2023-02-26 19:54:12 +0530

    Time: 59 ms (20.17%), Space: 81.5 MB (8.52%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    
+    int solve(string str1, string str2, int i, int j, vector<vector<int>> &dp){
+        if(i<0)
+            return j+1;
+        if(j<0)
+            return i+1;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        if(str1[i] == str2[j])
+            return dp[i][j] = solve(str1, str2, i-1, j-1, dp);
+        
+        return dp[i][j] = 1 + min(solve(str1, str2, i-1, j-1, dp), min(solve(str1, str2, i-1, j, dp),solve(str1, str2, i, j-1, dp)));
+    }
+    
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return solve(word1, word2, n-1, m-1, dp);
+    }
+};
