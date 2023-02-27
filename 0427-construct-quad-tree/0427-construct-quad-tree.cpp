71d88315b95b7ab093e3374809c44eeb6aa07124
Mudar Hussain <mudar.shussain@gmail.com>
2023-02-27 09:05:40 +0530

    Time: 28 ms (14.49%), Space: 22.6 MB (32.61%) - LeetHub

---
+++ 0427-construct-quad-tree/0427-construct-quad-tree.cpp
@@ -0,0 +1,110 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+    
+    Node() {
+        val = false;
+        isLeaf = false;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = NULL;
+        topRight = NULL;
+        bottomLeft = NULL;
+        bottomRight = NULL;
+    }
+    
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+*/
+
+class Solution {
+    Node* solve(vector<vector<int>> &grid, int n, int i, int j){
+        Node* root = new Node(grid[i][j], true);
+        if(n == 1){
+            return root;
+        }
+        
+        int subSize = n/2;
+        root->topLeft = solve(grid, subSize, i, j);
+        root->topRight = solve(grid, subSize, i, j+subSize);
+        root->bottomLeft = solve(grid, subSize, i+subSize, j);
+        root->bottomRight = solve(grid, subSize, i+subSize, j+subSize);
+        
+        bool allLeaf = (root->topLeft->isLeaf == 1 && root->topRight->isLeaf == 1 && root->bottomLeft->isLeaf == 1 && root->bottomRight->isLeaf == 1);
+        bool allValEq = (root->topLeft->val == root->topRight->val 
+                         && root->topRight->val == root->bottomLeft->val
+                         && root->bottomLeft->val == root->bottomRight->val);
+        
+        if(allLeaf && allValEq){
+                    
+            root->topLeft = NULL;
+            root->topRight = NULL;
+            root->bottomLeft = NULL;
+            root->bottomRight = NULL;
+            
+        }else
+            root->isLeaf = false;
+            
+        
+        return root;
+    }
+    
+public:
+    Node* construct(vector<vector<int>>& grid) {
+        int n = grid.size();
+        
+        return solve(grid, n, 0, 0);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
