353c8b8ff96b677596259e18469edba9a7a8e416
Mudar Hussain <mudar.shussain@gmail.com>
2023-03-01 16:51:07 +0530

    Time: 480 ms (25.10%), Space: 133.8 MB (20.31%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    void merge(vector<int> &nums, int l, int mid, int h){
+        
+        int i = l;
+        int j = mid+1;
+        int k = 0;
+        vector<int> v(h-l+1);
+        
+        while(i<=mid && j<=h){
+            if(nums[i]<=nums[j]){
+                v[k++] = nums[i++];
+            }else{
+                v[k++] = nums[j++];
+            }
+        }
+        while(i<=mid){
+            v[k++] = nums[i++];
+        }
+        
+        while(j<=h){
+            v[k++] = nums[j++];
+        }
+        
+        for(int i = 0; i<k; i++){
+            nums[l+i] = v[i];
+        }
+    }
+    
+    void mergeSort(vector<int> &nums, int l, int h){
+        if(l>=h)
+            return;
+        int mid = l + (h-l)/2;
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid+1, h);
+        merge(nums, l, mid, h);
+        
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size()-1);
+        return nums;
+    }
+};
