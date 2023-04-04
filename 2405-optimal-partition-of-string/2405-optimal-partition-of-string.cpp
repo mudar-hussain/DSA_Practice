32d7ed9c48dffa62692c9f01f31af7a8c5a330b0
Mudar Hussain <mudar.shussain@gmail.com>
2023-04-04 11:17:39 +0530

    Time: 24 ms (88.36%), Space: 10.3 MB (77.04%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,28 +1,37 @@
 class Solution {
-    vector<int> dp;
-    int solve(string& s, int &n, int idx){
-        if(idx >= n){
-            return 0;
-        }
-        if(dp[idx] != -1)
-            return dp[idx];
+//     vector<int> dp;
+//     int solve(string& s, int &n, int idx){
+//         if(idx >= n){
+//             return 0;
+//         }
+//         if(dp[idx] != -1)
+//             return dp[idx];
         
-        int curr = INT_MAX;
-        vector<int> v(26,0);
-        for(int i = idx; i<n; i++){
-            if(v[s[i]-'a'] == 1)
-                break;
-            v[s[i] - 'a'] = 1;
-            curr = min(curr, 1+solve(s, n, i+1));
-        }
-        return dp[idx] = curr;
+//         int curr = INT_MAX;
+//         vector<int> v(26,0);
+//         for(int i = idx; i<n; i++){
+//             if(v[s[i]-'a'] == 1)
+//                 break;
+//             v[s[i] - 'a'] = 1;
+//             curr = min(curr, 1+solve(s, n, i+1));
+//         }
+//         return dp[idx] = curr;
         
-    }
+//     }
 public:
     int partitionString(string s) {
         int n = s.length();
-        dp.resize(n+1, -1);
+        int dp[26];
+        memset(dp, 0, sizeof(dp));
+        int ans = 0;
+        for(char c: s){
+            if(dp[c-'a'] != 0){
+                ans++;
+                memset(dp, 0, sizeof(dp));
+            }
+            dp[c-'a']++;
+        }
         
-        return solve(s, n, 0);
+        return ans+1;
     }
 };
