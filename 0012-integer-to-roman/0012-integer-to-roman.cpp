6ac630f50f415f70e3e2040bb55a13ddef1cba47
Mudar Hussain <mudar.shussain@gmail.com>
2023-04-12 05:14:00 +0530

    Time: 12 ms (39.93%), Space: 9.5 MB (12.24%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,27 +1,29 @@
 class Solution {
-    unordered_map<int, string> um;
 public:
     string intToRoman(int num) {
-    um[1] = "I";
-    um[4] = "IV";
-    um[5] = "V";
-    um[9] = "IX";
-    um[10] = "X";
-    um[40] = "XL";
-    um[50] = "L";
-    um[90] = "XC";
-    um[100] = "C";
-    um[400] = "CD";
-    um[500] = "D";
-    um[900] = "CM";
-    um[1000] = "M";
-        int values[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        vector<pair<int, string>> v ={
+            {1,"I"},
+            {4,"IV"},
+            {5,"V"},
+            {9, "IX"},
+            {10, "X"},
+            {40, "XL"},
+            {50, "L"},
+            {90, "XC"},
+            {100, "C"},
+            {400, "CD"},
+            {500, "D"},
+            {900, "CM"},
+            {1000, "M"},
+        };
+        
         string ans = "";
-        for(int i = 0; num>0 && i<13; i++){
-            if(num>=values[i]){
-                ans += um[values[i]];
-                num -= values[i];
-                i--;
+        
+        for(int i = 12; num>0 && i>=0; i--){
+            if(num>=v[i].first){
+                ans += v[i].second;
+                num -= v[i].first;
+                i++;
             }
         }
         return ans;
