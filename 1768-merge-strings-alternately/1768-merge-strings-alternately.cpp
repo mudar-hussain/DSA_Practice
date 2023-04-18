bbbc04a7f3ab52eb39140436417d8619d3fe72e1
Mudar Hussain <mudar.shussain@gmail.com>
2023-04-18 07:12:51 +0530

    Time: 4 ms (38.78%), Space: 6.3 MB (60.87%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i = 0, j = 0;
+        string ans = "";
+        while(i<word1.length() && j<word2.length()){
+            ans += word1[i++];
+            ans += word2[j++];
+        }
+        while(i<word1.length()){
+            ans += word1[i++];
+        }
+        while(j<word2.length()){
+            ans += word2[j++];
+        }
+        return ans;
+    }
+};
