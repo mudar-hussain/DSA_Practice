cf56d8fb442dcfeeb129b262d503115b0c4495b5
Mudar Hussain <mudar.shussain@gmail.com>
2023-05-12 06:48:53 +0530

    Time: 409 ms (15.51%), Space: 123.1 MB (34.83%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,20 +1,19 @@
 class Solution {
-    long long dp[100001];
-    long long solve(vector<vector<int>> &q, int idx){
+    long long solve(vector<vector<int>> &q, vector<long long> &dp, int idx){
         if(idx>=q.size())
             return 0;
         if(dp[idx] != -1){
             return dp[idx];
         }
-        long long a = solve(q, idx+1);
-        long long b = q[idx][0]+solve(q, idx+q[idx][1]+1);
+        long long a = solve(q, dp, idx+1);
+        long long b = q[idx][0]+solve(q, dp, idx+q[idx][1]+1);
         
         return dp[idx] = max(a, b);
     }
     
 public:
     long long mostPoints(vector<vector<int>>& questions) {
-        memset(dp, -1, sizeof(dp));
-        return solve(questions, 0);
+        vector<long long> dp(questions.size()+1, -1);
+        return solve(questions, dp, 0);
     }
 };
