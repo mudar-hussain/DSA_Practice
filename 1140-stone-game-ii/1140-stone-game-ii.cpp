1d7773b0a72c8b3b5321bc484c10ecbd60524cfc
Mudar Hussain <mudar.shussain@gmail.com>
2023-05-26 06:18:40 +0530

    Time: 13 ms (84.84%), Space: 10.1 MB (63.56%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int n;
+    vector<int> p;
+    vector<int> p_sum;
+    vector<vector<int>> dp;
+    
+    int stoneGameII(vector<int>& p) 
+    {
+        this->n = p.size();
+        this->p = p;
+        
+        dp.resize(n, vector<int>(n, -1));
+        
+        p_sum.resize(n+1, 0);
+        for(int i = 1 ; i <= n ; ++i) {
+            p_sum[i] = p_sum[i-1] + p[i-1];
+        }
+        
+        return solve(0, 1);
+    }
+    int solve(int index, int m)
+    {
+        if(index >= n) {
+        return 0;            
+        }
+        if(index + 2*m >= n){
+        return p_sum[n] - p_sum[index];
+        } 
+        if(dp[index][m] != -1){
+        return dp[index][m];
+        } 
+        
+        int ans = INT_MAX;
+        for(int i = 1 ; i <= 2*m ; ++i)
+        {
+            ans = min(ans, solve(index + i, max(i, m)));
+        }
+        return dp[index][m] = (p_sum[n] - p_sum[index] - ans);
+    }
+    
+};
