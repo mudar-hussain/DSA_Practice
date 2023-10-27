311da38eac487b76c8ca911369165bc274e771fa
Mudar Hussain <mudar.shussain@gmail.com>
2023-10-27 07:47:54 +0530

    Time: 1 ms (75.67%), Space: 44.6 MB (17.51%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,29 +1,12 @@
 class Solution {
     public int trap(int[] height) {
-        int [] ngl = getNgl(height);
-        int[] ngr = getNgr(height);
-        int ans = 0;
-        for(int i = 0; i<height.length; i++){
-            ans = ans + Math.max(Math.min(ngl[i], ngr[i]) - height[i], 0);
+        int l = 0, r = height.length-1, ans = 0;
+        int lmax = Integer.MIN_VALUE, rmax = Integer.MIN_VALUE;
+        while(l<r){
+            lmax = Math.max(lmax, height[l]);
+            rmax = Math.max(rmax, height[r]);
+            ans += (lmax<rmax)?lmax-height[l++]:rmax-height[r--];
         }
         return ans;
-    }
-    private int[] getNgl(int[] height){
-        int[] ngl = new int[height.length];
-        int maxi = 0;
-        for(int i = 0; i<height.length; i++){
-            ngl[i] = maxi;
-            maxi = Math.max(maxi, height[i]);
-        }
-        return ngl;
-    }
-    private int[] getNgr(int[] height){
-        int[] ngr = new int[height.length];
-        int maxi = 0;
-        for(int i = height.length-1; i>=0; i--){
-            ngr[i] = maxi;
-            maxi = Math.max(maxi, height[i]);
-        }
-        return ngr;
     }
 }
